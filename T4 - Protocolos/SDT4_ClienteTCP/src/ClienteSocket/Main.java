8069ac6b7780ea061b50e9536db78d23e04d1626
Mirko Ochoa <superenano1514@gmail.com>
2023-04-18 17:43:02 -0400

    Chat_TCP_final

--- T4 - Protocolos/SDT4_ClienteTCP/src/ClienteSocket/Main.java
+++ T4 - Protocolos/SDT4_ClienteTCP/src/ClienteSocket/Main.java
@@ -10,6 +10,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ClienteTCP cliente = new ClienteTCP("127.0.0.1", 5000);
+        try{
+            ClienteTCP cliente = new ClienteTCP(args[0], Integer.parseInt(args[1]));
+        }catch(Exception ex){
+            ClienteTCP cliente = new ClienteTCP("127.0.0.1", 5000);
+        }
+        
     }
 }
